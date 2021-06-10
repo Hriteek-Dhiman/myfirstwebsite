1b832eeb4337cef45953615a78354bc26cf76117
hriteek <hriteekdhimanom@gmail.com>
2021-06-10 08:27:56 +0530

    My first page.

---
+++ site/result.js
@@ -0,0 +1,388 @@
+function hy()
+{ 
+var q1=document.quiz.question1.value;
+  if(q1=="Disagree Strongly")
+    {
+      var a=1;
+         }
+  if(q1=="Disagree Moderately")
+    {
+      var a=2;
+    }
+  if(q1=="Disagree a little")
+    {
+      var a=3;
+    }
+  if(q1=="neither agree nor disagree")
+    {
+      var a=4;
+    }
+  if(q1=="Agree a little")
+    {
+      var a=5;
+    }
+  if(q1=="Agree Moderately")
+    {
+      var a=6;
+    }
+  if(q1=="Agree strongly")
+    {
+      var a=7;
+    }
+var q2=document.quiz.question2.value;
+  if(q2=="Disagree Strongly")
+    {
+      var z=7;
+     
+    }
+  if(q2=="Disagree Moderately")
+    {
+      var z=6;
+    }
+  if(q2=="Disagree a little")
+    {
+      var z=5;
+    }
+  if(q2=="neither agree nor disagree")
+    {
+      var z=4;
+    }
+  if(q2=="Agree a little")
+    {
+      var z=3;
+    }
+  if(q2=="Agree Moderately")
+    {
+      var z=2;
+    }
+  if(q2=="Agree strongly")
+    {
+      var z=1;
+    }
+var q3=document.quiz.question3.value;
+  if(q3=="Disagree Strongly")
+    {
+      var c=1;
+    
+    }
+  if(q3=="Disagree Moderately")
+    {
+      var c=2;
+    }
+  if(q3=="Disagree a little")
+    {
+      var c=3;
+    }
+  if(q3=="neither agree nor disagree")
+    {
+      var c=4;
+    }
+  if(q3=="Agree a little")
+    {
+      var c=5;
+    }
+  if(q3=="Agree Moderately")
+    {
+      var c=6;
+    }
+  if(q3=="Agree strongly")
+    {
+      var c=7;
+    }
+var q4=document.quiz.question4.value;
+  if(q4=="Disagree Strongly")
+    {
+      var d=7;
+     
+    }
+  if(q4=="Disagree Moderately")
+    {
+      var d=6;
+    }
+  if(q4=="Disagree a little")
+    {
+      var d=5;
+    }
+  if(q4=="neither agree nor disagree")
+    {
+      var d=4;
+    }
+  if(q4=="Agree a little")
+    {
+      var d=3;
+    }
+  if(q4=="Agree Moderately")
+    {
+      var d=2;
+    }
+  if(q4=="Agree strongly")
+    {
+      var d=1;
+    }
+var q5=document.quiz.question5.value;
+  if(q5=="Disagree Strongly")
+    {
+      var e=1;
+    
+    }
+  if(q5=="Disagree Moderately")
+    {
+      var e=2;
+    }
+  if(q5=="Disagree a little")
+    {
+      var e=3;
+    }
+  if(q5=="neither agree nor disagree")
+    {
+      var e=4;
+    }
+  if(q5=="Agree a little")
+    {
+      var e=5;
+    }
+  if(q5=="Agree Moderately")
+    {
+      var e=6;
+    }
+  if(q5=="Agree strongly")
+    {
+      var e=7;
+    }
+var q6=document.quiz.question6.value;
+  if(q6=="Disagree Strongly")
+    {
+      var f=7;
+     
+    }
+  if(q6=="Disagree Moderately")
+    {
+      var f=6;
+    }
+  if(q6=="Disagree a little")
+    {
+      var f=5;
+    }
+  if(q6=="neither agree nor disagree")
+    {
+      var f=4;
+    }
+  if(q6=="Agree a little")
+    {
+      var f=3;
+    }
+  if(q6=="Agree Moderately")
+    {
+      var f=2;
+    }
+  if(q6=="Agree strongly")
+    {
+      var f=1;
+    }
+var q7=document.quiz.question7.value;
+  if(q7=="Disagree Strongly")
+    {
+      var g=1;
+    
+    }
+  if(q7=="Disagree Moderately")
+    {
+      var g=2;
+    }
+  if(q7=="Disagree a little")
+    {
+      var g=3;
+    }
+  if(q7=="neither agree nor disagree")
+    {
+      var g=4;
+    }
+  if(q7=="Agree a little")
+    {
+      var g=5;
+    }
+  if(q7=="Agree Moderately")
+    {
+      var g=6;
+    }
+  if(q7=="Agree strongly")
+    {
+      var g=7;
+    }
+var q8=document.quiz.question8.value;
+  if(q8=="Disagree Strongly")
+    {
+      var h=7;
+     
+    }
+  if(q8=="Disagree Moderately")
+    {
+      var h=6;
+    }
+  if(q8=="Disagree a little")
+    {
+      var h=5;
+    }
+  if(q8=="neither agree nor disagree")
+    {
+      var h=4;
+    }
+  if(q8=="Agree a little")
+    {
+      var h=3;
+    }
+  if(q8=="Agree Moderately")
+    {
+      var h=2;
+    }
+  if(q8=="Agree strongly")
+    {
+      var h=1;
+    }
+var q9=document.quiz.question9.value;
+  if(q9=="Disagree Strongly")
+    {
+      var i=1;
+    
+    }
+  if(q9=="Disagree Moderately")
+    {
+      var i=2;
+    }
+  if(q9=="Disagree a little")
+    {
+      var i=3;
+    }
+  if(q9=="neither agree nor disagree")
+    {
+      var i=4;
+    }
+  if(q9=="Agree a little")
+    {
+      var i=5;
+    }
+  if(q9=="Agree Moderately")
+    {
+      var i=6;
+    }
+  if(q9=="Agree strongly")
+    {
+      var i=7;
+    }
+var q10=document.quiz.question10.value;
+  if(q10=="Disagree Strongly")
+    {
+      var j=7;
+     
+    }
+  if(q10=="Disagree Moderately")
+    {
+      var j=6;
+    }
+  if(q10=="Disagree a little")
+    {
+      var j=5;
+    }
+  if(q10=="neither agree nor disagree")
+    {
+      var j=4;
+    }
+  if(q10=="Agree a little")
+    {
+      var j=3;
+    }
+  if(q10=="Agree Moderately")
+    {
+      var j=2;
+    }
+  if(q10=="Agree strongly")
+    {
+      var j=1;
+    }
+   
+    var ee=(a+f);
+    var a=(z+g);
+    var c=(c+h);
+    var es=(d+i);
+    var o=(e+j); 
+var me=ee/2;
+var ma=a/2;
+var mc=c/2;
+var mes=es/2;
+var mo=o/2;
+var gme=4.44;
+var gma=5.23;
+var gmc=5.40;
+var gmes=4.83;
+var gom=5.38;
+var rmo=mo-gom;
+//document.write(rmo);
+if(rmo>=0 && rmo<1.07)
+document.write("<h1>Openness trait</h1> --><h2> Slightly Above Average</h2>");
+if(rmo>=1.07 && rmo<2.14)
+document.write("Openness trait --> Above Average");
+if( rmo>=2.14)
+document.write("Openness trait --> High");
+if(rmo<=0 && rmo>-1.07)
+document.write("Openness trait --> Slightly Below Average");
+if(rmo>=-2.14 && rmo<=-1.07)
+document.write("Openness trait --> Below Average");
+if( rmo<-2.14)
+document.write("Openness trait --> Low");
+
+var rem=me-gme;
+if(rem>=0 && rem<1.45)
+document.write(" Extraversion trait --> Slightly Above Average");
+if(rem>=1.45 && rem<2.90)
+document.write("Extraversion trait -->  Above Average");
+if( rem>=2.90)
+document.write("Extraversion trait --> High");
+if(rem<=0 && rem>-1.45)
+document.write("Extraversion trait --> Slightly Below Average");
+if(rem<=-1.45 && rem>=-2.90)
+document.write("Extraversion trait --> Below Average");
+if( rem<-2.90)
+document.write("Extraversion trait --> Low");
+
+var ram=ma-gma;
+if(ram>=0 && ram<1.11)
+document.write("Agreeableness Trait --> Slightly Above Average");
+if(ram>=1.11 && ram<2.22)
+document.write("Agreeableness Trait --> Above Average");
+if( ram>=2.22)
+document.write("Agreeableness Trait --> High");
+if(ram<=0 && ram>-1.11)
+document.write("Agreeableness Trait --> Slightly Below Average");
+if(ram<=-1.11 && ram>=-2.22)
+document.write("Agreeableness Trait --> Below Average");
+if( ram<-2.22)
+document.write("Agreeableness Trait --> Low");
+
+var rcm=mc-gmc;
+if(rcm>=0 && rcm<1.32)
+document.write(" Conscientiousness Trait --> Slightly above average");
+if(rcm>=1.32 && rcm<2.64)
+document.write("Conscientiousness Trait --> Above average");
+if( rcm>=2.64)
+document.write("Conscientiousness Trait --> High");
+if(rcm<=0 && rcm>-1.32)
+document.write("Conscientiousness Trait --> Slightly below average");
+if(rcm<=-1.32 && rcm>=-2.64)
+document.write("Conscientiousness Trait --> Below average");
+if( rcm<-2.64)
+document.write("Conscientiousness Trait --> Low");
+
+var rnm=mes-gmes;
+if(rnm>=0 && rnm<1.42)
+document.write("Emotional Stability Trait --> Slightly above Average");
+if(rnm>=1.42 && rnm<2.84)
+document.write("Emotional Stability Trait --> Above Average");
+if( rnm>2.48)
+document.write("Emotional Stability Trait --> High");
+if(rnm<=0 && rnm>-1.42)
+document.write("Emotional Stability Trait --> Slightly below Average");
+if(rnm<=-1.42 && rnm>=-2.84)
+document.write("Emotional Stability Trait --> Below Average");
+if( rnm<-2.84)
+document.write("Emotional Stability Trait --> Low");
+}
